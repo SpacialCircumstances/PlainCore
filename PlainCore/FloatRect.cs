4cf928716a9558bf0a3f7f38b38e06893ed65e0d
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-09-08 18:06:33 +0200

    Create structs for integer/float rectangles

---
+++ PlainCore/FloatRect.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace PlainCore
+{
+    public struct FloatRect
+    {
+        public FloatRect(Vector2 position, Vector2 size)
+        {
+            Position = position;
+            Size = size;
+        }
+
+        public FloatRect(float x, float y, float w, float h)
+        {
+            Position = new Vector2(x, y);
+            Size = new Vector2(w, h);
+        }
+
+        public Vector2 Position { get; }
+        public Vector2 Size { get; }
+        public Vector2 Corner => Position + Size;
+        public float X => Position.X;
+        public float Y => Position.Y;
+        public float Width => Size.X;
+        public float Height => Size.Y;
+        public float Left => Position.X;
+        public float Top => Position.Y;
+        public float Bottom => Position.Y + Size.Y;
+        public float Right => Position.X + Size.X;
+
+        public override bool Equals(object obj)
+        {
+            return obj is FloatRect rect &&
+                   Position.Equals(rect.Position) &&
+                   Size.Equals(rect.Size);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1930765514;
+            hashCode = hashCode * -1521134295 + EqualityComparer<Vector2>.Default.GetHashCode(Position);
+            hashCode = hashCode * -1521134295 + EqualityComparer<Vector2>.Default.GetHashCode(Size);
+            return hashCode;
+        }
+    }
+}
---
+++ PlainCore/IntRect.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlainCore
+{
+    public struct IntRect
+    {
+        public IntRect(int x, int y, int width, int height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+
+        public int X { get; }
+        public int Y { get; }
+        public int Width { get; }
+        public int Height { get; }
+        public int Left => X;
+        public int Right => X + Width;
+        public int Top => Y;
+        public int Bottom => Y + Height;
+
+        public override bool Equals(object obj)
+        {
+            return obj is IntRect rect &&
+                   X == rect.X &&
+                   Y == rect.Y &&
+                   Width == rect.Width &&
+                   Height == rect.Height;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 466501756;
+            hashCode = hashCode * -1521134295 + X.GetHashCode();
+            hashCode = hashCode * -1521134295 + Y.GetHashCode();
+            hashCode = hashCode * -1521134295 + Width.GetHashCode();
+            hashCode = hashCode * -1521134295 + Height.GetHashCode();
+            return hashCode;
+        }
+    }
+}
