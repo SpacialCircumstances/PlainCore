bda0ac65bd7516f0e9df5f6e7a81495fbf4f0e10
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-09-08 21:07:46 +0200

    Add tests for ArrayBufferList

---
+++ PlainCore.Test/ArrayBufferListTest.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+
+namespace PlainCore.Test
+{
+    public class ArrayBufferListTest
+    {
+        [Fact]
+        public void AddingItems()
+        {
+            var list = new ArrayBufferList<string>();
+            list.Add("test");
+            list.Add("test2");
+            list.Add("test3");
+            list.Add("test4");
+            Assert.Equal(4, list.Count);
+            list.Add("test5");
+            Assert.Equal(5, list.Count);
+            list.Clear();
+            Assert.Empty(list);
+            list.Add("test0");
+            list[0] = "test_test";
+            Assert.Single(list);
+            Assert.Equal("test_test", list[0]);
+        }
+
+        [Fact]
+        public void Resizing()
+        {
+            var list = new ArrayBufferList<string>(4);
+            Assert.Equal(4, list.Capacity);
+            Assert.Equal(4, list.Buffer.Length);
+            
+            for (int i = 0; i < 12; i++)
+            {
+                list.Add($"test{i}");
+            }
+
+            Assert.Equal(12, list.Count);
+            Assert.True(list.Capacity >= 12);
+            Assert.True(list.Buffer.Length >= 12);
+            list.Clear();
+            Assert.True(list.Capacity >= 12);
+        }
+    }
+}
