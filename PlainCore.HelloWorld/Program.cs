916ad4c2b6a2bb0e36eb24ad81ab194065783fb3
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-09-08 17:53:04 +0200

    Create example project

---
+++ PlainCore.HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlainCore.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
