b94847a18c30b582f62265ccc879765685096b97
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-09-09 21:52:59 +0200

    Fix

--- PlainCore/SpriteRenderItem.cs
+++ PlainCore/SpriteRenderItem.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using PlainCore.Vertices;
 
 namespace PlainCore
 {
     public struct SpriteRenderItem
     {
-        public SpriteRenderItem(VertexPositionColorTexture topLeft, VertexPositionColorTexture topRight, VertexPositionColorTexture bottomLeft, VertexPositionColorTexture bottomRight, Texture2D texture)
+        public SpriteRenderItem(VertexPosition3ColorTexture topLeft, VertexPosition3ColorTexture topRight, VertexPosition3ColorTexture bottomLeft, VertexPosition3ColorTexture bottomRight, Texture2D texture)
         {
             TopLeft = topLeft;
             TopRight = topRight;
@@ -15,29 +16,29 @@ namespace PlainCore
             Texture = texture;
         }
 
-        public VertexPositionColorTexture TopLeft { get; }
-        public VertexPositionColorTexture TopRight { get; }
-        public VertexPositionColorTexture BottomLeft { get; }
-        public VertexPositionColorTexture BottomRight { get; }
+        public VertexPosition3ColorTexture TopLeft { get; }
+        public VertexPosition3ColorTexture TopRight { get; }
+        public VertexPosition3ColorTexture BottomLeft { get; }
+        public VertexPosition3ColorTexture BottomRight { get; }
         public Texture2D Texture { get; }
 
         public override bool Equals(object obj)
         {
             return obj is SpriteRenderItem item &&
-                   EqualityComparer<VertexPositionColorTexture>.Default.Equals(TopLeft, item.TopLeft) &&
-                   EqualityComparer<VertexPositionColorTexture>.Default.Equals(TopRight, item.TopRight) &&
-                   EqualityComparer<VertexPositionColorTexture>.Default.Equals(BottomLeft, item.BottomLeft) &&
-                   EqualityComparer<VertexPositionColorTexture>.Default.Equals(BottomRight, item.BottomRight) &&
+                   EqualityComparer<VertexPosition3ColorTexture>.Default.Equals(TopLeft, item.TopLeft) &&
+                   EqualityComparer<VertexPosition3ColorTexture>.Default.Equals(TopRight, item.TopRight) &&
+                   EqualityComparer<VertexPosition3ColorTexture>.Default.Equals(BottomLeft, item.BottomLeft) &&
+                   EqualityComparer<VertexPosition3ColorTexture>.Default.Equals(BottomRight, item.BottomRight) &&
                    EqualityComparer<Texture2D>.Default.Equals(Texture, item.Texture);
         }
 
         public override int GetHashCode()
         {
             var hashCode = 1905714886;
-            hashCode = hashCode * -1521134295 + EqualityComparer<VertexPositionColorTexture>.Default.GetHashCode(TopLeft);
-            hashCode = hashCode * -1521134295 + EqualityComparer<VertexPositionColorTexture>.Default.GetHashCode(TopRight);
-            hashCode = hashCode * -1521134295 + EqualityComparer<VertexPositionColorTexture>.Default.GetHashCode(BottomLeft);
-            hashCode = hashCode * -1521134295 + EqualityComparer<VertexPositionColorTexture>.Default.GetHashCode(BottomRight);
+            hashCode = hashCode * -1521134295 + EqualityComparer<VertexPosition3ColorTexture>.Default.GetHashCode(TopLeft);
+            hashCode = hashCode * -1521134295 + EqualityComparer<VertexPosition3ColorTexture>.Default.GetHashCode(TopRight);
+            hashCode = hashCode * -1521134295 + EqualityComparer<VertexPosition3ColorTexture>.Default.GetHashCode(BottomLeft);
+            hashCode = hashCode * -1521134295 + EqualityComparer<VertexPosition3ColorTexture>.Default.GetHashCode(BottomRight);
             hashCode = hashCode * -1521134295 + EqualityComparer<Texture2D>.Default.GetHashCode(Texture);
             return hashCode;
         }
