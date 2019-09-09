13a9c13c9f3a33f29e18cb3e827cc5cf179661cb
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-09-09 18:40:53 +0200

    Add class for encapsulating a texture for sprite renderign

---
+++ PlainCore/Texture2D.cs
@@ -0,0 +1,22 @@
+﻿using Veldrid;
+
+namespace PlainCore
+{
+    public class Texture2D
+    {
+        public static Texture2D FromTextureAndView(Texture texture, TextureView textureView)
+        {
+            return new Texture2D
+            {
+                Texture = texture,
+                TextureView = textureView
+            };
+        }
+
+        public Texture Texture { get; private set; }
+        public TextureView TextureView { get; private set; }
+
+        public int Height => (int)Texture.Height;
+        public int Width => (int)Texture.Width;
+    }
+}
