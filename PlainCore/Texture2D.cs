9a2f16a735dda117753b2924a5e7dfac2e1de9b3
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-09-10 15:58:47 +0200

    Textures get unique ids to allow for batching

--- PlainCore/Texture2D.cs
+++ PlainCore/Texture2D.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.Threading;
 using Veldrid;
 
 namespace PlainCore
 {
     public class Texture2D
     {
+        private static int textureIdCounter = 0;
+
         public static Texture2D FromTextureAndView(Texture texture, TextureView textureView)
         {
             return new Texture2D
@@ -13,12 +16,18 @@ namespace PlainCore
                 TextureView = textureView
             };
         }
+        private Texture2D()
+        {
+            this.Id = textureIdCounter;
+            Interlocked.Increment(ref textureIdCounter);
+        }
 
         public Texture Texture { get; private set; }
         public TextureView TextureView { get; private set; }
 
         public int Height => (int)Texture.Height;
         public int Width => (int)Texture.Width;
+        public int Id { get; private set; }
 
         public override bool Equals(object obj)
         {
