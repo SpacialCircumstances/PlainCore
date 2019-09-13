0b1328ad8345e29d7a134ce6f92d750bd691e59a
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-09-13 16:08:03 +0200

    Fix shader syntax errors

--- PlainCore/Shaders.cs
+++ PlainCore/Shaders.cs
@@ -13,13 +13,13 @@ namespace PlainCore
             layout(location = 2) in vec2 texCoords;
             layout(set = 0, binding = 0) uniform WorldView {
                 mat4 viewMatrix;
-            }
+            };
 
             layout(location = 0) out vec4 fsColor;
             layout(location = 1) out vec2 fsTexCoords;
             
             void main() {
-                gl_Position = viewMatrix * vec4(position, 1)
+                gl_Position = viewMatrix * vec4(position, 1);
                 fsColor = color;
                 fsTexCoords = texCoords;
             }
