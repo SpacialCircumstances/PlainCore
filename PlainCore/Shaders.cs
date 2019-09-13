8baf021b1ae0869d63a3a000597eb7f532098fe8
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-09-13 16:15:24 +0200

    Make names in shaders match vertex layout

--- PlainCore/Shaders.cs
+++ PlainCore/Shaders.cs
@@ -8,9 +8,9 @@ namespace PlainCore
     {
         public const string SPRITEBATCH_DEFAULT_VERTEX_SHADER = @"
             #version 450
-            layout(location = 0) in vec3 position;
-            layout(location = 1) in vec4 color;
-            layout(location = 2) in vec2 texCoords;
+            layout(location = 0) in vec3 Position;
+            layout(location = 1) in vec4 Color;
+            layout(location = 2) in vec2 TextureCoordinates;
             layout(set = 0, binding = 0) uniform WorldView {
                 mat4 viewMatrix;
             };
@@ -19,16 +19,16 @@ namespace PlainCore
             layout(location = 1) out vec2 fsTexCoords;
             
             void main() {
-                gl_Position = viewMatrix * vec4(position, 1);
-                fsColor = color;
-                fsTexCoords = texCoords;
+                gl_Position = viewMatrix * vec4(Position, 1);
+                fsColor = Color;
+                fsTexCoords = TextureCoordinates;
             }
         ";
 
         public const string SPRITEBATCH_DEFAULT_FRAGMENT_SHADER = @"
             #version 450
             layout(location = 0) in vec4 fsColor;
-            layout(location = 1) in vec2 texCoords;
+            layout(location = 1) in vec2 fsTexCoords;
 
             layout(set = 1, binding = 0) uniform texture2D Texture;
             layout(set = 1, binding = 1) uniform sampler TextureSampler;
@@ -36,7 +36,7 @@ namespace PlainCore
             layout(location = 0) out vec4 outColor;
 
             void main() {
-                vec4 texColor = texture(sampler2D(Texture, TextureSampler), texCoords);
+                vec4 texColor = texture(sampler2D(Texture, TextureSampler), fsTexCoords);
                 outColor = texColor * fsColor;
             }
         ";
