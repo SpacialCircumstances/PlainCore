b24a4e5769b4ff27bf925612aae21e852a819271
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-10-01 22:47:15 +0200

    Add TextureRegion for keeping a texture with a source rectangle

---
+++ PlainCore/TextureRegion.cs
@@ -0,0 +1,28 @@
+﻿namespace PlainCore
+{
+    public class TextureRegion
+    {
+        public TextureRegion(FloatRect region, Texture2D texture)
+        {
+            Region = region;
+            Texture = texture;
+        }
+
+        public FloatRect Region { get; }
+        public Texture2D Texture { get; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is TextureRegion region &&
+                   Region.Equals(region.Region) &&
+                   Texture.Equals(region.Texture);
+        }
+        public override int GetHashCode()
+        {
+            var hashCode = -51722311;
+            hashCode = hashCode * -1521134295 + Region.GetHashCode();
+            hashCode = hashCode * -1521134295 + Texture.GetHashCode();
+            return hashCode;
+        }
+    }
+}
