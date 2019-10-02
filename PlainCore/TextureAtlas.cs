274f484f4bf6d71974ece53e72f47ac8b5ff922d
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-10-02 22:27:02 +0200

    Add TextureAtlas

---
+++ PlainCore/TextureAtlas.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlainCore
+{
+    public class TextureAtlas
+    {
+        private readonly Dictionary<string, TextureRegion> atlas;
+
+        public static TextureAtlas Create(Texture2D texture, IDictionary<string, IntRect> regions)
+        {
+            var textureRegions = regions.Select(entry =>
+            {
+                if (entry.Value.Left >= 0 &&
+                    entry.Value.Top >= 0 &&
+                    entry.Value.Right <= texture.Width &&
+                    entry.Value.Bottom <= texture.Height)
+                {
+                    return new KeyValuePair<string, TextureRegion>(entry.Key, new TextureRegion(entry.Value, texture));
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(nameof(regions), "One region is outside the bounds of the texture");
+                }
+            }).ToDictionary(e => e.Key, e => e.Value);
+            return new TextureAtlas(textureRegions);
+        }
+
+        protected TextureAtlas(Dictionary<string, TextureRegion> atlas)
+        {
+            this.atlas = atlas;
+        }
+
+        public TextureRegion this[string name]
+        {
+            get => atlas[name];
+        }
+    }
+}
