92c743839f7c8cda8f28af3a7dd4201d9aa5bbfc
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-10-03 17:32:55 +0200

    Add interface for asset loaders

---
+++ PlainCore.Content/IAssetLoader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Text.Json;
+
+namespace PlainCore.Content
+{
+    public interface IAssetLoader
+    {
+        bool IsSupported(Type type);
+        object Load(JsonElement parameters, Type type, string rootDirectory);
+    }
+}
