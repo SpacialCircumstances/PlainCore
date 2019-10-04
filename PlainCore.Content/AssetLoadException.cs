22a28d718f4b7cd330ea024184355c9803928015
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-10-04 17:11:43 +0200

    Add asset loader for Texture2D

---
+++ PlainCore.Content/AssetLoadException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PlainCore.Content
+{
+    public class AssetLoadException: Exception
+    {
+        public AssetLoadException(string message) : base(message)
+        {
+        }
+
+        public AssetLoadException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public AssetLoadException()
+        {
+        }
+    }
+}
---
+++ PlainCore.Content/Texture2DLoader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace PlainCore.Content
+{
+    public class Texture2DLoader : IAssetLoader
+    {
+        public bool IsSupported(Type type)
+        {
+            return type == typeof(Texture2D);
+        }
+
+        public object Load(ContentLoader contentLoader, JsonElement parameters, Type type)
+        {
+            try 
+            {
+                var pathElementValue = parameters.GetProperty("path").GetString();
+                var mipmap = parameters.GetPropertyOrDefault("mipmap", (el) => el.GetBoolean(), () => true);
+                var srgb = parameters.GetPropertyOrDefault("srgb", (el) => el.GetBoolean(), () => false);
+                var path = Path.Combine(contentLoader.RootDirectory, pathElementValue);
+                return Texture2D.FromFile(contentLoader, path, mipmap, srgb);
+            } 
+            catch(Exception e)
+            {
+                throw new AssetLoadException($"Failed to load asset of type {type.Name}: {e.Message}", e);
+            }
+        }
+    }
+}
