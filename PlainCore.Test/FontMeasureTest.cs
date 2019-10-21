b3af4b88f5b0a0c15bbb7a3f235c041278382cb8
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-10-21 22:23:02 +0200

    More tests for font measuring

--- PlainCore.Test/FontMeasureTest.cs
+++ PlainCore.Test/FontMeasureTest.cs
@@ -23,10 +23,15 @@ namespace PlainCore.Test
             AddGlyph(glyphDict, new GlyphLayout('a', null, new Vector2(10.0f, 10.0f), new Vector2(0.0f, 0.0f), 3.0f));
             AddGlyph(glyphDict, new GlyphLayout('b', null, new Vector2(8.0f, 10.0f), new Vector2(0.0f, 0.0f), 4.0f));
             AddGlyph(glyphDict, new GlyphLayout('g', null, new Vector2(5.0f, 11.0f), new Vector2(0.5f, 1.0f), 5.0f));
+            AddGlyph(glyphDict, new GlyphLayout('!', null, new Vector2(3.0f, 12.0f), new Vector2(0.0f, -1.0f), 2.0f));
             var glyphs = new Glyphs(glyphDict);
             var fontDescription = new FontDescription(fontMetadata, bitmap, glyphs);
             Assert.Equal(new Vector2(6.0f, 10.0f), fontDescription.MeasureString("aa"));
             Assert.Equal(new Vector2(12.0f, 20.0f), fontDescription.MeasureString("aa", 2.0f));
+            Assert.Equal(new Vector2(7.0f, 10.0f), fontDescription.MeasureString("ab"));
+            Assert.Equal(new Vector2(17.0f, 12.0f), fontDescription.MeasureString("abgg"));
+            Assert.Equal(new Vector2(25.0f, 30.0f), fontDescription.MeasureString("gg", 2.5f));
+            Assert.Equal(new Vector2(4.0f, 11.0f), fontDescription.MeasureString("!!"));
         }
     }
 }
